7730e36e27c5c701a922c6eebad5f4d887fc8651
gui lima <gb8752061@gmail.com>
2023-10-05 13:55:09 -0300

    Configurando o ambiente de desenvolvimento

---
+++ src/App.tsx
@@ -0,0 +1,15 @@
+
+import Teste from './Teste'
+ import EstiloGlobal from './styles'
+
+function App() {
+
+  return (
+    <>
+    <EstiloGlobal />
+    <Teste />
+    </>
+    )
+}
+
+export default App
